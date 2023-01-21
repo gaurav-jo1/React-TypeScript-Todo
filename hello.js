cc4959d8087a9c43813644f2e326913c3fb62ac9
Gaurav Joshi <gj451026@gmail.com>
2023-01-21 13:06:38 +0530

    Defining a union type

--- hello.js
+++ hello.js
@@ -1,3 +1,22 @@
 // 1.Type Annotations on Variables
 var myName = "Alice";
-console.log(myName);
+// 2.FUnctions
+function greet(name) {
+    console.log("Hello, ".concat(name.toUpperCase(), "!!"));
+}
+// Return Type Annotations
+function addition(first, second) {
+    return first + second;
+}
+var result = addition(5, 3);
+// Anonymous Functions
+var names = ["Alice", "Bob", "Eve"];
+names.forEach(function (name) {
+    console.log(name.toUpperCase());
+});
+// Object Types
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
--- hello.ts
+++ hello.ts
@@ -1,6 +1,52 @@
-// 1.Type Annotations on Variables
+// ➡ 1.Type Annotations on Variables
 
 const myName: string = "Alice";
 
+// ➡ 2.FUnctions
 
-console.log(myName)
+function greet(name: string) {
+  console.log(`Hello, ${name.toUpperCase()}!!`);
+}
+
+
+// ➡ Return Type Annotations
+
+function addition(first: number, second: number): number {
+  return first + second;
+}
+
+let result = addition(5,3)
+
+// ➡ Anonymous Functions
+const names = ["Alice", "Bob", "Eve"];
+
+names.forEach((name) => {
+  console.log(name.toUpperCase())
+})
+
+// ➡ Object Types
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+// ➡ Optional Properties
+
+function printName(obj: { first: string; last?: string }) {
+  // ...
+}
+// Both OK
+
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+
+// ➡ Defining a Union Type
+
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
